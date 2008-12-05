d0b15cbbd3d103bbe34af780c0aa3d7fab06d645
sundaramurthis <sundaramurthis@17208e7a-a90f-11dd-8af2-493fb0bb9f7c>
2008-12-05 15:20:19 +0000

    wsdl parser impl

---
+++ jsonwebservice/src/com/jaxws/json/feature/JSONWebServiceFeature.java
@@ -0,0 +1,12 @@
+package com.jaxws.json.feature;
+
+import javax.xml.ws.WebServiceFeature;
+
+public class JSONWebServiceFeature extends WebServiceFeature {
+
+	@Override
+	public String getID() {
+		return "com.jaxws.json.feature.JSONWebServiceFeature";
+	}
+
+}
