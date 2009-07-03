c52a1d5add7d876d008e37a147a4a465c31ef019
sundaramurthis <sundaramurthis@17208e7a-a90f-11dd-8af2-493fb0bb9f7c>
2009-07-03 14:42:33 +0000

    for backup

--- trunk/jsonwebservice/src/com/jaxws/json/codec/doc/JSONHttpMetadataPublisher.java
+++ trunk/jsonwebservice/src/com/jaxws/json/codec/doc/JSONHttpMetadataPublisher.java
@@ -1,9 +1,15 @@
 package com.jaxws.json.codec.doc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -104,9 +110,7 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 						}
 					}
 					jsonIn.append("}}");
-					
 					//Out
-					
 					jsonOut.append("{");
 					countParam = 0;
 					for(ParameterImpl param:methodImpl.getResponseParameters()){
@@ -154,66 +158,117 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 			
 			out.write(templateMain.getBytes());
 			return true;
-		}else if(queryString.startsWith("form")){
-			StringBuffer form = new StringBuffer();
+		}else if(queryString.startsWith("model") || queryString.startsWith("client")){
+			StringBuffer model = new StringBuffer();
 			connection.setStatus(HttpURLConnection.HTTP_OK);
 			connection.setContentTypeResponseHeader("application/json;charset=\"utf-8\"");
 	        
 			OutputStream out = connection.getOutput();
 			
-			form.append("{\""+endPoint.getServiceName().getLocalPart()+"\":{");
-			form.append("\""+endPoint.getPortName().getLocalPart()+"\":{");
+			model.append("{\""+endPoint.getServiceName().getLocalPart()+"\":{");
+			
+			model.append("\""+endPoint.getPortName().getLocalPart()+"\":{");
 			byte count 	= 0;
 			for(JavaMethod method:endPoint.getSEIModel().getJavaMethods()){
 				JavaMethodImpl methodImpl = (JavaMethodImpl) method;
 				if(count != 0){
-					form.append(",");
+					model.append(",");
 				}
 				count++;
-				form.append("\""+methodImpl.getOperationName()+"\":{");
-				form.append("\"input\":{");
-				int countParam = 0;
-				for(ParameterImpl param:methodImpl.getRequestParameters()){
-					if(param instanceof WrapperParameter){
-						WrapperParameter wparam =(WrapperParameter)param;
-						for(ParameterImpl paramChild :wparam.getWrapperChildren()){
-							if(countParam >0){
-								form.append(",");
+				model.append("\""+methodImpl.getOperationName()+"\":{");
+				
+				if(queryString.startsWith("client")){
+					model.append("\"input\":{");
+					int countParam = 0;
+					for(ParameterImpl param:methodImpl.getRequestParameters()){
+						if(param instanceof WrapperParameter){
+							WrapperParameter wparam =(WrapperParameter)param;
+							for(ParameterImpl paramChild :wparam.getWrapperChildren()){
+								if(countParam >0){
+									model.append(",");
+								}
+								model.append("\""+paramChild.getName().getLocalPart()+"\":");
+								serializeBean((Class<?>) paramChild.getTypeReference().type,model,new ArrayList<Class<?>>(),queryString.indexOf("autoBind") > 0);
+								countParam ++;
 							}
-							form.append("\""+paramChild.getName().getLocalPart()+"\":");
-							serializeBean((Class<?>) paramChild.getTypeReference().type,form,new ArrayList<Class<?>>(),queryString.indexOf("autoBind") > 0);
+						}else{
+							model.append("\""+param.getName().getLocalPart()+"\":");
+							serializeBean((Class<?>) param.getTypeReference().type,model,new ArrayList<Class<?>>(),queryString.indexOf("autoBind") > 0);
 							countParam ++;
 						}
-					}else{
-						form.append("\""+param.getName().getLocalPart()+"\":");
-						serializeBean((Class<?>) param.getTypeReference().type,form,new ArrayList<Class<?>>(),queryString.indexOf("autoBind") > 0);
-						countParam ++;
 					}
-				}
-				form.append("},");
-				form.append("\"invoke\":function(callback){");
-				/// AJAX invoker
-				String url = null;
-				if(endPoint.getPort().getAddress().getURL() ==null){
-					url = connection.getBaseAddress()+ adapter.getValidPath();
+					model.append("}");
 				}else{
-					url = endPoint.getPort().getAddress().getURL().toExternalForm();
+					/// Type metadata
+					model.append("\"input\":{");
+					int countParam = 0;
+					for(ParameterImpl param:methodImpl.getRequestParameters()){
+						if(param instanceof WrapperParameter){
+							WrapperParameter wparam =(WrapperParameter)param;
+							for(ParameterImpl paramChild :wparam.getWrapperChildren()){
+								if(countParam >0){
+									model.append(",");
+								}
+								model.append("\""+paramChild.getName().getLocalPart()+"\":");
+								serializeMetaDtata((Class<?>) paramChild.getTypeReference().type,model,new ArrayList<Class<?>>());
+								countParam ++;
+							}
+						}else{
+							model.append("\""+param.getName().getLocalPart()+"\":");
+							serializeMetaDtata((Class<?>) param.getTypeReference().type,model,new ArrayList<Class<?>>());
+							countParam ++;
+						}
+					}
+					model.append("},");
+					model.append("\"config\":{");
+					model.append("\"endPoint\":\""+endPoint.getPort().getAddress().getURL().getPath()+"\",");
+					model.append("\"requestContentType\":\"application/json\",");//TODO read from codec config
+					model.append("\"responseContentType\":\"application/json\",");//TODO read from method config
+					model.append("\"zipedResponse\":\"false\",");
+					model.append("\"statusAware\":\"false\",");//TODO read from method config
+					model.append("\"statusUrl\":\"null\",");//TODO read from method config
+					model.append("\"featureUrl\":\"null\",");//TODO read from method config
+					model.append("\"requestPayload\":\""+methodImpl.getOperationName()+"\"");//TODO read from method config
+					model.append("}");
 				}
-				form.append(
-						"new Ajax.Request('"+url+"',{"+
-					 " method:'post',"+
-					 " requestHeaders: {Accept: 'application/json'},"+
-					 " postBody: '{\""+methodImpl.getOperationName()+"\":'+Object.toJSON(this.input)+'}',"+
-					 " onComplete: callback"+
-					"});}");
-
-				
-				///
-				form.append("}");
+				//
+				if(queryString.startsWith("client")){
+					model.append(",\"output\":\"TODO?? MAy be place holder for result\"");
+				//Outmeta data
+				}else {
+					model.append(",\"output\":{");
+					int countParam = 0;
+					for(ParameterImpl param:methodImpl.getResponseParameters()){
+						if(param instanceof WrapperParameter){
+							WrapperParameter wparam =(WrapperParameter)param;
+							for(ParameterImpl paramChild :wparam.getWrapperChildren()){
+								if(countParam >0){
+									model.append(",");
+								}
+								model.append("\""+paramChild.getName().getLocalPart()+"\":");
+								serializeMetaDtata((Class<?>) paramChild.getTypeReference().type,model,new ArrayList<Class<?>>());
+								countParam ++;
+							}
+						}else{
+							model.append("\""+param.getName().getLocalPart()+"\":");
+							serializeMetaDtata((Class<?>) param.getTypeReference().type,model,new ArrayList<Class<?>>());
+							countParam ++;
+						}
+					}
+					model.append("}");
+				}
+				//Type metadata
+				//Method close
+				model.append("}");
 			}
-			form.append("}}}");
+			model.append("}"); // Port end	
+			if(queryString.startsWith("client")){
+				model.append(convertStreamToString(getClass().getResourceAsStream("client.js")));
+			}
+			model.append("}");// Service end
+			model.append("}");
 			//serializeBean(endPoint.getImplementationClass(),form,new ArrayList<Class<?>>());
-			out.write(form.toString().getBytes());
+			out.write(model.toString().getBytes());
 			return true;
 		}
 		return false;
@@ -221,8 +276,10 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 	
 	private void serializeBean(Class<?> bean,StringBuffer out,List<Class<?>> stack,boolean autoBind) throws IOException{
 		for(Class<?> stackBean : stack ){
-			if(stackBean.equals(bean))//Recursion deducted
+			if(stackBean.equals(bean)){//Recursion deducted
+				out.append("null");
 				return;
+			}
 		}
 		if(bean.isEnum()){
 			out.append("\""+bean.getSimpleName()+"\"");
@@ -249,9 +306,8 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 						}else{
 							out.append("{");
 						}
-						
 						try{
-						Class.forName("com.jaxws.json.JaxWsJSONPopulator");
+							Class.forName("com.jaxws.json.JaxWsJSONPopulator");
 						}catch(Throwable th){
 							th.printStackTrace();
 						}
@@ -260,15 +316,17 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 							if(field.getType().getName().equals(JAXBElement.class.getName())){
 								//TODO serialize element
 								//serializeBean(field.getType(), out);
-								out.append("\"\"");
+								out.append("null");
 							}else{
 								stack.add(bean);
 								serializeBean(field.getType(), out,stack,autoBind);
+								stack.remove(bean);
 							}
 						}else{
 							XmlElement xmlElemnt = field.getAnnotation(XmlElement.class);
 							if(autoBind){
-								out.append("\""+escapeString(field.getName())+"\":function(){try{return $(\""+escapeString(field.getName())+"\").getValue().toJSON();}catch(e){}}");
+								//out.append("\""+escapeString(field.getName())+"\":function(){try{return $(\""+escapeString(field.getName())+"\").getValue().toJSON();}catch(e){}}");
+								out.append("\""+escapeString(field.getName())+"\":function(){try{return JSON_BIND_ACTIVE_FORM."+escapeString(field.getName())+".value}catch(e){return null;}}");
 							}else{
 								out.append("\""+escapeString(field.getName())+"\":\""+(xmlElemnt != null?xmlElemnt.defaultValue() != null ?xmlElemnt.defaultValue().trim():"":"") +"\"");
 							}
@@ -283,7 +341,7 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 					bean.getDeclaredAnnotations();
 					out.append("\""+(new JSONPopulator().convert(bean, null, null, null))+"\"");
 				}else{
-					out.append("\"\"");
+					out.append("null");
 				}
 			}
 		}catch(Throwable th){
@@ -292,12 +350,169 @@ public class JSONHttpMetadataPublisher extends HttpMetadataPublisher {
 		}
 	}
 	
+	private void serializeMetaDtata(Class<?> bean,StringBuffer out,List<Class<?>> stack) throws IOException{
+		for(Class<?> stackBean : stack ){
+			if(stackBean.equals(bean)){//Recursion deducted
+				out.append("null");
+				return;
+			}
+		}
+		if(bean.isEnum()){
+			out.append("[");
+			int size = bean.getEnumConstants().length;
+			for(Object cnst:bean.getEnumConstants()){
+				out.append("\""+cnst.toString()+"\""+(size-- == 0?"":","));
+			}
+			out.append("]");
+			return;
+		}
+		if(JaxWsJSONPopulator.isJSONPrimitive(bean)){
+			//out.append("\"\"");
+			out.append("{");
+			out.append("\"type\":\""+bean.getSimpleName()+"\",");
+			out.append("\"nillable\":\"false\",");
+			out.append("\"required\":\"true\"");
+			out.append("}");
+			return;
+		}
+		try{
+			if(bean != null){
+				int count =0;
+				nextField:
+				for(Field field:bean.getDeclaredFields()){
+					field.getAnnotations();
+					for(Pattern patten:JSONCodec.excludeProperties){
+						if(patten.matcher(field.getName()).matches()){
+							continue nextField;
+						}
+					}
+					if(field.getDeclaringClass().getName().equals(bean.getName())){
+						if(count != 0 ){
+							out.append(",");
+						}else{
+							out.append("{");
+						}
+						try{
+							Class.forName("com.jaxws.json.JaxWsJSONPopulator");
+						}catch(Throwable th){
+							th.printStackTrace();
+						}
+						if(field.getType() instanceof Class && !JaxWsJSONPopulator.isJSONPrimitive(field.getType())){
+							out.append("\""+escapeString(field.getName())+"\":");
+							if(field.getType().getName().equals(JAXBElement.class.getName())){
+								//TODO serialize element
+								//serializeBean(field.getType(), out);
+								out.append("null");
+							}else{
+								stack.add(bean);
+								serializeMetaDtata(field.getType(), out,stack);
+								stack.remove(bean);
+							}
+						}else{
+							XmlElement xmlElemnt = field.getAnnotation(XmlElement.class);
+							out.append("\""+escapeString(field.getName())+"\":{");
+							// Generate object meta data
+							if(xmlElemnt != null){
+								out.append("\"defaultValue\":");
+								string(xmlElemnt.defaultValue(),out);
+								out.append(", \"nillable\":\""+xmlElemnt.nillable()+"\"");
+								out.append(", \"required\":\""+xmlElemnt.required()+"\"");// TODO difrence betwin nillable and required
+								out.append(", \"type\":\""+field.getType().getSimpleName()+"\"");
+								out.append(", \"restriction\":{");
+									out.append("\"minLength\":0");
+									out.append(",");
+									out.append("\"maxLength\":255");
+									out.append(",\"pattern\":\"\"");
+								out.append("}");
+								
+							}
+							out.append("}");
+							
+							//		"\""+(xmlElemnt != null?xmlElemnt.defaultValue() != null ?xmlElemnt.defaultValue().trim():"":"") +"\"");
+						}
+					}
+					count++;
+				}
+				if(count != 0 ){
+					out.append("}");
+				}else if(bean.isPrimitive()){
+					bean.getAnnotations();
+					bean.getDeclaredAnnotations();
+					out.append("\""+(new JSONPopulator().convert(bean, null, null, null))+"\"");
+				}else{
+					out.append("null");
+				}
+			}
+		}catch(Throwable th){
+			th.printStackTrace();
+			out.append(th.getMessage());
+		}
+	}
 	private String escapeString(String input){
 		try{
 			return input.replaceAll("\\$", "");
+		}catch(NullPointerException exp){
+			return "null";
 		}catch(Throwable th){
 			return input;
 		}
 	}
 	
+	private void string(Object obj, StringBuffer out) {
+		out.append('"');
+		CharacterIterator it = new StringCharacterIterator(obj.toString());
+		for (char c = it.first(); c != CharacterIterator.DONE; c = it.next()) {
+			if (c == '"') {
+				out.append("\\\"");
+			} else if (c == '\\') {
+				out.append("\\\\");
+			} else if (c == '/') {
+				out.append("\\/");
+			} else if (c == '\b') {
+				out.append("\\b");
+			} else if (c == '\f') {
+				out.append("\\f");
+			} else if (c == '\n') {
+				out.append("\\n");
+			} else if (c == '\r') {
+				out.append("\\r");
+			} else if (c == '\t') {
+				out.append("\\t");
+			} else if (Character.isISOControl(c)) {
+				// this.unicode(c);
+			} else {
+				out.append(c);
+			}
+		}
+		out.append('"');
+	}
+	
+	public String convertStreamToString(InputStream is) {
+        /*
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
