6a1c573e0a659d3913e623c3bc8c4684c2c0c841
sundaramurthis <sundaramurthis@17208e7a-a90f-11dd-8af2-493fb0bb9f7c>
2009-08-03 15:54:06 +0000

    custom content type base response handler added

--- trunk/jsonwebservice/src/com/jaxws/json/codec/JSONContentType.java
+++ trunk/jsonwebservice/src/com/jaxws/json/codec/JSONContentType.java
@@ -2,8 +2,8 @@ package com.jaxws.json.codec;
 
 import com.sun.xml.ws.api.pipe.ContentType;
 
-final class JSONContentType implements ContentType{
-	static final String JSON_MIME_TYPE 		= "application/json";
+public final class JSONContentType implements ContentType{
+	public static final String JSON_MIME_TYPE 		= "application/json";
 
     public String getContentType() {
         return JSON_MIME_TYPE;
