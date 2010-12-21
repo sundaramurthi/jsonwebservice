e749947cbd1a4a2f9db6cd5a5d6fee476b88164a
sundaramurthis <sundaramurthis@17208e7a-a90f-11dd-8af2-493fb0bb9f7c>
2010-12-21 13:27:38 +0000

    hello world updated with index html file

--- trunk/demos/helloWorld/src/com/mycompany/HelloWorldImpl.java
+++ trunk/demos/helloWorld/src/com/mycompany/HelloWorldImpl.java
@@ -1,15 +1,18 @@
 package com.mycompany;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
-
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 @WebService (name="HelloService", targetNamespace="http://album.jsonplugin.com/json/")
 public class HelloWorldImpl {
 
-	@WebMethod (operationName="sayHello")
-	public @WebResult(name="message") String sayHello(String name){
+	@WebMethod (operationName = "sayHello")
+	public @WebResult(name="message") String sayHello(
+			@WebParam(name="name") String name){
 		return "Hello "+name;
 	}
 }
