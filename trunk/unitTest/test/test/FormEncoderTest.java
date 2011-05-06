12ba63f247c811a1aab1938190cf1cdae3db60e4
sundaramurthis@gmail.com <sundaramurthis@gmail.com@17208e7a-a90f-11dd-8af2-493fb0bb9f7c>
2011-05-06 12:50:27 +0000

    jquery test index page added and 0.7 in class path

---
+++ trunk/unitTest/test/test/FormEncoderTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class FormEncoderTest extends JSONCodecTest /*implements com.test.jsonwebservice.parameter.ParameterTest*/{
+	
+	protected void setUp() throws Exception {
+		END_POINT = "http://localhost:8080/unitTest/json/parameter";
+		super.setUp();
+	}
+
+}
